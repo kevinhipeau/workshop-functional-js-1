bf7b3e88fbf4bdb1b72a339fd174b57f46ed6537
Kévin <kevin.hipeau@gmail.com>
2017-04-21 09:56:19 +0200

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -2,10 +2,7 @@ const chalk = require('chalk');
 
 console.log(chalk.cyan('\nEXERCISE 3 LOADED\n'));
 
-const reverseString = (input) => {
-
-  // Your future job begins here ...
-}
+const reverseString = (input) => (input === "") ?  "" :  reverseString(input.substr(1)) + input.charAt(0);
 
 module.exports = {
   reverseString: reverseString,
